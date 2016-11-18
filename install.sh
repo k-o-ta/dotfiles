0200dac9f49a980d0e4ef2c0c0312cc66258468b
Koji Ota <afterjnih@gmail.com>
2016-11-18 22:09:06 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zshenv ~/.zshenv
 ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
 mkdir -p ~/.vim/ftplugin
+mkdir -p ~/.vim/snippets
 ln -sf ~/dotfiles/vim/ftplugin/rust.vim ~/.vim/ftplugin/rust.vim
+ln -sf ~/dotfiles/vim/snippets/org.snip ~/.vim/snippets/org.snip
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
